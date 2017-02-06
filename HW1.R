43f96228c8f0491ecbbcc51ccaf5eea877cdc4c5
Michael Amireh <mma38e@gmail.com>
2017-02-06 15:30:17 -0800

    HW 1 solutions

--- HW1.R
+++ HW1.R
@@ -1,1 +1,13 @@
+rm(list = ls())
+# 0
 print("Hello World")
+# 1
+print (seq(1,10,1))
+# sol 2
+  for(i in 1:10){
+    print (sprintf("%d",i))
+  }
+# 2
+pnorm(0)
+# 3 determine 1 in P(X,q) = .025
+qnorm(0.025,0.1)
---
+++ HW2.R
@@ -0,0 +1,2 @@
+# 1 get 1000 observations from popoulation with distribtuion N(0,4)
+# 2 calculate proportion of observations that are greater than 2
