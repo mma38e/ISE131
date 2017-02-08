583560eacfcdf8992a03963969e0cace29b0571e
Michael Amireh <mma38e@gmail.com>
2017-02-08 15:18:33 -0800

    added R3

--- HW2.R
+++ HW2.R
@@ -1,2 +1,4 @@
 # 1 get 1000 observations from popoulation with distribtuion N(0,4)
+df = rnorm(1000, mean = 4, sd = 1)
+df
 # 2 calculate proportion of observations that are greater than 2
---
+++ R3.R
@@ -0,0 +1,9 @@
+# task Draw a unit circle centered at (0,0) in the following way
+# 1. Let 0 = 2pi/5
+# 2. start from (1,0). sweeping the circle in the counter clockwisedirection, plot a point on the circle at increments of 0. (connect the consecutive points with a line)
+# 3. Add a title to the plot
+# 4 do dstep 1,2 & 3 again but change divisor from step 1 from 5 to 50 and then 500
+plot(,,type = "l",...)
+cos()
+sin()
+rep()
