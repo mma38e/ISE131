c0a3c07c0ccdc323ff1d0c8df606437823f11eca
Michael Amireh <mma38e@gmail.com>
2017-02-13 15:09:53 -0800

    Added teacher solution for R2

--- HW2.R
+++ HW2.R
@@ -1,4 +1,21 @@
 # 1 get 1000 observations from popoulation with distribtuion N(0,4)
-rnorm(1000, mean = 0, sd = 4)
-pnorm
+observations <- rnorm(1000, mean = 0, sd = 2)
 # 2 calculate proportion of observations that are greater than 2
+mydf <- data.frame(x = observations) # save observations in a dataframe
+
+proportion = sum(mydf > 2)/1000
+proportion
+prop.table(mydf)
+
+
+#Professor Solution
+rm(list = ls())
+value =rnorm(1000,0,2)
+count = 0
+for(index in 1:1000){
+  if(value[index]>2){
+    count = count +1
+  }
+  }
+proportion = count/1000
+print(sprintf("Proportion = %f",proportion))
--- R3.R
+++ R3.R
@@ -3,7 +3,13 @@
 # 2. start from (1,0). sweeping the circle in the counter clockwisedirection, plot a point on the circle at increments of 0. (connect the consecutive points with a line)
 # 3. Add a title to the plot
 # 4 do dstep 1,2 & 3 again but change divisor from step 1 from 5 to 50 and then 500
-plot(,,type = "l",...)
-cos()
-sin()
-rep()
+# plot(,,type = "l",...)
+# cos()
+# sin()
+# rep()
+#plot(1:5,seq(1,10,length=5),type="l",xlab="",ylab="",main="Test draw.circle")
+rm(list=ls())
+require(plotrix)
+plot(-1:1,-1:1,type = "l",main = "Circle")
+draw.circle(1,0,.5,nv=100,border=NULL,col=NA,lty=1,lwd=1)
+
