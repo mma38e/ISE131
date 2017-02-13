50e62c4bfca68bb7f36156e2f43b0bd9456712ae
Michael Amireh <mma38e@gmail.com>
2017-02-13 15:23:19 -0800

    added R4 assignment

---
+++ R4.R
@@ -0,0 +1,25 @@
+# #R4 assignment
+# *function*
+# function()
+# choose()
+# list()
+# factorial
+# 
+# *concept*
+# list
+# 
+# *packages*
+# readxl
+# EnvStats
+# 
+# *Tasks*
+#Create a function cp(n,r,type)
+#cp() calculates Combination if "type" contains "comb"
+#cp() calculates permutations if "type" contains "perm"
+#cp() calculates permutations if "type" not specified
+#cp() returns a list that has the following components
+  #n from the argument n
+  #r from the arguement r
+  #type "comb" for Cn/r
+        #"perm" for Pn/r
+  #value comb or perm, depending on "type"
